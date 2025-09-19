f80f2ebbde90cba0760a89ad4d633405314be45a
prudhvi1519 <prudhviakula92@gmail.com>
2025-09-19 18:53:01 +0530

    Frontend route file updated

--- debtprotection-quiz/src/components/form/MultiStepForm.jsx
+++ debtprotection-quiz/src/components/form/MultiStepForm.jsx
@@ -473,7 +473,7 @@ export default function MultiStepForm() {
   setSubmitting(true);
   setSubmitError("");
   try {
-    const res = await fetch("http://debtprotection.org/api/form/submit", {
+    const res = await fetch("https://debtprotection.org/api/form/submit", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
