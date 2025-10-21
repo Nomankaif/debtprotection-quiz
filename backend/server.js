089e4de20084849ad0066654c45c866cd273fb60
visakotipavankumar047 <pavankumarvisakoti047@gmail.com>
2025-10-21 16:55:25 +0530

    changed /quiz on backend

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,8 @@ app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => res.send("Form API is running ??"));
-app.use("quiz/api/form", formRoutes);
+app.use("/quiz/api/form", formRoutes);
+
 
 // Catch-all 404 Handler for debugging
 app.use((req, res, next) => {
